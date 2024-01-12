24342c45ba0959187db839613a9c56890eb62c97
soyelmejorjajaka <adrian.teransanchez@iesmiguelherrero.com>
2024-01-12 17:02:52 +0100

    pues eso

---
+++ src/main/java/com/mycompany/aaaaa/Aaaaa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aaaaa;
+
+/**
+ *
+ * @author DAW109
+ */
+public class Aaaaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
