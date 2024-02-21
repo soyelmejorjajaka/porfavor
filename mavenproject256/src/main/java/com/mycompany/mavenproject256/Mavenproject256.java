6bb362057e9f510d5c477bcc63ef05e9b1f4f6d3
soyelmejorjajaka <adrian.teransanchez@iesmiguelherrero.com>
2024-02-21 16:13:41 +0100

    GFRTDRGFTSRH

---
+++ mavenproject256/src/main/java/com/mycompany/mavenproject256/Mavenproject256.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject256;
+
+/**
+ *
+ * @author DAW109
+ */
+public class Mavenproject256 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //HASTALAPOLLA
+    }
+}
