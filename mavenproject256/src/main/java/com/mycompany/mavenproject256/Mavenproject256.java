65e785074b1525979fa34ce3362fbf1b30e86032
soyelmejorjajaka <153939533+soyelmejorjajaka@users.noreply.github.com>
2024-02-22 17:43:48 +0100

    Update Mavenproject256.java

--- mavenproject256/src/main/java/com/mycompany/mavenproject256/Mavenproject256.java
+++ mavenproject256/src/main/java/com/mycompany/mavenproject256/Mavenproject256.java
@@ -11,7 +11,7 @@ package com.mycompany.mavenproject256;
 public class Mavenproject256 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //HASTALAPOLLAMECAGONTUPUTAMADRE
+        System.out.println("Hello rld!");
+        //HASTALAPOLLAMECAGONTTAMADRE
     }
 }
